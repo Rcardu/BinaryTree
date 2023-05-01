1520002b437a623ea0936465f5c2902b3d6aaf75
ricardo <gq1090523745@163.com>
2023-05-01 10:39:44 +0800

    深度优先遍历（递归）

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include"BiaryTrees\\RecursiveTraversalOfBinaryTrees.cpp"
+
+
+void RecursiveTraversalOfBinaryTreesFormat();
+
+int main(){
+       RecursiveTraversalOfBinaryTreesFormat();
+
+
+    while(getchar()!='\n')
+    continue;
+    getchar();
+    return 0;
+}
