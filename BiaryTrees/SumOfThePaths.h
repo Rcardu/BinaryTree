af307cc4fe8ae5e2c610e0028f4535ec2f58015a
ricardo <gq1090523745@163.com>
2023-05-06 00:02:46 +0800

    路径和等于目标和

--- BiaryTrees/SumOfThePaths.h
+++ BiaryTrees/SumOfThePaths.h
@@ -15,6 +15,8 @@ public:
     /*给定一个目标和，判断该树中是否存在根节点到叶子节点的路径上，所有的节点值相加等于目标和*/
     /*递归版,先序*/
     bool SumOfThePaths_Arec(TreeNode*node,int count);
+    /*迭代版*/
+    bool SumOfThePaths_Apro(TreeNode*node,int count);
 };
 /*目标和==路径和，深度优先遍历
 1.确定递归函数及返回值：需要根节点指针，一个int计数器来判断一条边上的总和是否等于目标和
@@ -48,6 +50,23 @@ bool SumOfThePaths::SumOfThePaths_Arec(TreeNode*node,int count){
     }
     if(node->right){
         if(SumOfThePaths_Arec(node->right,count-node->right->val))return true;
+    }
+    return false;
+}
+/*使用栈进行先序遍历*/
+bool SumOfThePaths::SumOfThePaths_Apro(TreeNode*node,int count){
+    if(node==NULL)return 0;
+    stack<pair<TreeNode*,int>>st;
+    //<节点，路径值>
+    st.push(pair<TreeNode*,int>(node,node->val));
+    while(!st.empty()){
+        pair<TreeNode*,int>ptr=st.top();
+        st.pop();
+        //如果该节点时叶子节点，就比较路径和是否满足条件
+        if(!ptr.first->left&&!ptr.first->right&&ptr.second==count)return true;
+        //右结点，压入一个节点的时候，将其路径也记录下来
+        if(ptr.first->right)st.push(pair<TreeNode*,int>(ptr.first->right,ptr.second+ptr.first->right->val));
+        if(ptr.first->left)st.push(pair<TreeNode*,int>(ptr.first->left,ptr.second+ptr.first->left->val));
     }
     return false;
 }
