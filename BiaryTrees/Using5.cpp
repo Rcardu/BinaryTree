fc0d614d78e18a433da1d9cab21f7a9a9a41f6e8
ricardo <gq1090523745@163.com>
2023-05-08 22:13:04 +0800

    搜索二叉树的最近公共祖先，插入，删除，构造，裁剪

--- BiaryTrees/Using5.cpp
+++ BiaryTrees/Using5.cpp
@@ -9,6 +9,8 @@
 #include<string>
 #endif
 #include"CommonAncestorSearchTree.h"
+#include"TrimBinaryTree.h"
+#include"CreatSearchTree.h"
 
 
 using namespace std;
@@ -16,6 +18,8 @@ using namespace std;
 void CommonAncestorSearchTreeFormat();//二叉搜索树的最近公共祖先
 void CommonAncestorSearchTree_InsertFormat();//二叉搜索树的插入
 void CommonAncestorSearchTree_DelteFormat();//二叉搜索树的删除
+void TrimBinaryTreeFormat();//裁剪搜索二叉树
+void CreatSearchTreeFormat();//构造搜索二叉树
 
 
 //二叉搜索树的最近公共祖先
@@ -55,6 +59,41 @@ void CommonAncestorSearchTree_DelteFormat(){
     CommonAncestorSearchTree obj;
     LayerOrderTraversal edu;
     vector<vector<int>>vec=edu.LayerOrderTraversal_acl(obj.CommonAncestorSearchTree_DeleteRec(cat.Construct_B(ionter,0,N,postrder,0,N),1));
+    for(int i=0;i<vec.size();i++){
+        for(int j=0;j<vec[i].size();j++){
+            cout<<vec[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+//裁剪搜索二叉树
+void TrimBinaryTreeFormat(){
+    int N;
+    cin>>N;
+    vector<int>ionder(N);
+    vector<int>postorder(N);
+    for(int i=0;i<N;i++)cin>>ionder[i];
+    for(int i=0;i<N;i++)cin>>postorder[i];
+    ConstructBinaryTree cat;
+    TrimBinaryTree obj;
+    LayerOrderTraversal edu;
+    vector<vector<int>>vec=edu.LayerOrderTraversal_acl(obj.TrimBinaryTree_Iter(cat.Construct_B(ionder,0,N,postorder,0,N),1,3));
+    for(int i=0;i<vec.size();i++){
+        for(int j=0;j<vec[i].size();j++){
+            cout<<vec[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+//构造搜索二叉树
+void CreatSearchTreeFormat(){
+    int N;
+    cin>>N;
+    vector<int>search(N);
+    for(int i=0;i<N;i++)cin>>search[i];
+    CreatSearchTree obj;
+    LayerOrderTraversal cat;
+    vector<vector<int>>vec=cat.LayerOrderTraversal_acl(obj.CreatSearchTree_Rec(search,0,search.size()-1));
     for(int i=0;i<vec.size();i++){
         for(int j=0;j<vec[i].size();j++){
             cout<<vec[i][j]<<" ";
